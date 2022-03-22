03a6ba28dbf6f8f1411ee1e16abdd823f3513908
Peizhao Li <peizhaol@linux-19.andrew.cmu.edu>
2022-03-21 23:00:37 -0400

    some basic fields and init_master

--- mycode/Server.java
+++ mycode/Server.java
@@ -1,10 +1,47 @@
 import java.io.*;
+import java.util.*;
+import java.rmi.*;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.server.UnicastRemoteObject;
+import java.util.concurrent.*;
 
-public class Server {
+public class Server extends UnicastRemoteObject {
 
+	// Request queue, maintained by master server
+	public static Queue request_queue;
+
+	// A list of frontend servers, containing VMIDs
+	public static Map<Integer, boolean> frontend_servers;
+
+	// A list of app servers, containing VMIDs
+	public static Map<Integer, boolean> app_servers;
+	
+
+
+	// Server load: request per server
+	// Request queue length
+	// Above may be good hints to decide scaling params
+	// Do not register the App tiers to the cloud
+	// Hyteresis: put some lag when changing the # of servers
+	// Time sample: do not observe too long or too short time for good # of new servers
+	// Be very careful with scaling-backs: actually add on double time
+	// CKPT2 arrival rates are also fixed
+	// Pay some attention to heavily tailed distribution
+	// Unhappy clients are more important?
     public Server() {
-
+		super();
     }
+
+	/**
+     * @brief Initialize master server, RMI registration, etc
+     * @return 1 on success, else errno
+     */
+	public static int init_master() {
+
+	}
+
+
+
 
 	public static void main ( String args[] ) throws Exception {
 		// if (args.length != 3) throw new Exception("Need 3 args: <cloud_ip> <cloud_port> <VM id>");
@@ -15,7 +52,7 @@ public class Server {
 			float current_time = SL.getTime();
 
 			if (current_time == 0) {
-				for (int i = 0; i < 7; i ++) {
+				for (int i = 0; i < 8; i ++) {
 					SL.startVM();
 				}
 			}
